ea686f8cc342a47b8fae144cbb44cca2dd782ad4
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-11 00:28:37 -0700

    Login test

--- index.php
+++ index.php
@@ -125,10 +125,14 @@ WHERE  email = '$username' and password = '$password';";
 
         <div class="illustration"><i  ></i><img class="benelogo" src="assets/img/benemerito.png" alt="Benemerito logo"></div>
         <br>
-        <div class="form-group"><input class="form-control" type="username" name="username" placeholder="Email"></div>
+        <div class="form-group"><input class="form-control" type="email" name="username" placeholder="Email"></div>
         <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
         <div class="form-group"><button class="btn btn-primary btn-block" name="login_btn" type="submit">Log In</button></div>
 
+</div>
+<input type="submit" name="login_btn" value="Login" class="btn btn-info btn-block">
+<div class="labelor"><b>Or</b></div>
+</div>
         <a class="forgot" href="#">Forgot your email or password?</a>
 
     </form>
