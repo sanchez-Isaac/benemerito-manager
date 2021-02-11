98e1f118f8b4282d06cce3be3cda873960fa5fea
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-11 00:03:57 -0700

    Login test

--- assets/views/teacher_Home.php
+++ assets/views/teacher_Home.php
@@ -21,10 +21,9 @@ echo '</pre>';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Untitled</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+
+
+
 </head>
 
 <body>
