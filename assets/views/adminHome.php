56967c7f791aeb4cc527e651e403725fd1a14f05
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-11 18:29:35 -0700

    adding style and grids to the admin and teacher pages

--- assets/views/adminHome.php
+++ assets/views/adminHome.php
@@ -38,18 +38,68 @@ echo '</pre>';
 <body>
 <title>Home Page</title>
 
-<div class="header">
-    <h1 class="headtitle"> Welcome <?php echo $_SESSION['username']. " " . $_SESSION['last_name'] ; ?></h1>
-    <br>
-    <br>
-</div>
-<p>Testing . Admin</p>
-
-
-
-
-
-
+<!-- Start: 1 Row 1 Column -->
+<div></div><!-- End: 1 Row 1 Column -->
+<!-- Start: 1 Row 3 Columns -->
+<div>
+    <div class="container">
+        <div class="row">
+            <!-- Start: school-logo -->
+            <div class="col-auto col-md-4 col-xl-2 offset-xl-0"></div><!-- End: school-logo -->
+            <!-- Start: profesor-admin-name -->
+            <div class="col-md-4 col-lg-8 col-xl-10 offset-xl-0"></div><!-- End: profesor-admin-name -->
+            <!-- Start: nav-bar -->
+            <div class="col-md-12 col-xl-12">
+                <!-- Start: Navigation with Button -->
+                <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
+                    <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="#"></a>
+                        <div class="collapse navbar-collapse" id="navcol-1">
+                            <ul class="nav navbar-nav mr-auto">
+                                <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Profile</a></li>
+                                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"></a>
+                                    <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
+                                </li>
+                            </ul><span class="navbar-text actions"> <a class="login" href="#">Log In</a><a class="btn btn-light action-button" role="button" href="#">Log Out</a></span>
+                        </div>
+                    </div>
+                </nav><!-- End: Navigation with Button -->
+            </div><!-- End: nav-bar -->
+        </div>
+    </div>
+</div><!-- End: 1 Row 3 Columns -->
+<!-- Start: 1 Row 3 Columns -->
+<div>
+    <div class="container">
+        <div class="row">
+            <!-- Start: part-of-grid1 -->
+            <div class="col-1 col-sm-1 col-md-1 col-lg-2 col-xl-2"></div><!-- End: part-of-grid1 -->
+            <!-- Start: table-of-classes-given -->
+            <div class="col-md-10 col-lg-8 col-xl-8"></div><!-- End: table-of-classes-given -->
+            <!-- Start: part-of-grid2 -->
+            <div class="col-1 col-sm-1 col-md-1 col-lg-2 col-xl-2"></div><!-- End: part-of-grid2 -->
+        </div>
+    </div>
+</div><!-- End: 1 Row 3 Columns -->
+<!-- Start: Footer Basic -->
+<div class="footer-basic">
+    <footer>
+        <!-- Start: Social Icons -->
+        <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div><!-- End: Social Icons -->
+        <!-- Start: Links -->
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Home</a></li>
+            <li class="list-inline-item"><a href="#">Services</a></li>
+            <li class="list-inline-item"><a href="#">About</a></li>
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        </ul><!-- End: Links -->
+        <!-- Start: Copyright -->
+        <p class="copyright">Company Name © 2017</p><!-- End: Copyright -->
+    </footer>
+</div><!-- End: Footer Basic -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
