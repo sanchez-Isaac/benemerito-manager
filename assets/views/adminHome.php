60f24cb9301d3f07c7c0183629d7b8314e6384b2
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-11 18:32:24 -0700

    adding style and grids to the admin and teacher pages

--- assets/views/adminHome.php
+++ assets/views/adminHome.php
@@ -30,7 +30,9 @@ echo '</pre>';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Untitled</title>
-
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
 
 
 </head>
