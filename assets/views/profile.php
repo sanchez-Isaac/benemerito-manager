ef56c6eca50ed46fd7ca4c15b12438832f5e7a75
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-24 13:35:34 -0700

    testing Javascript location

--- assets/views/profile.php
+++ assets/views/profile.php
@@ -35,7 +35,7 @@ echo '</pre>';
     }
     ?>
 
-
+    <script  src="/assets/js/jquery.min.js"></script>
 
 
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
@@ -188,7 +188,8 @@ echo '</pre>';
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"></path>
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"></path>
-                        </svg>Edit Profile
+                        </svg>
+                        Edit Profile
                     </button>
 
 
@@ -331,7 +332,7 @@ echo '</pre>';
     </footer>
 </div><!-- End: Footer Basic -->
 <script src="/assets/js/jquery.min.js"></script>
-
+<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
 <script  src="/assets/js/editProfile.js"></script>
 </body>
 
