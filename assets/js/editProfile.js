6c6931fb60816151a0bc59217af2c24b433fe73c
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-24 13:32:52 -0700

    testing Javascript location

--- assets/js/editProfile.js
+++ assets/js/editProfile.js
@@ -1,7 +1,7 @@
 function disableMyText(){
     if(document.getElementById("flexCheckUser_name").checked == true){
-        document.getElementById("recipient-name").disabled = false;
-    }else{
         document.getElementById("recipient-name").disabled = true;
+    }else{
+        document.getElementById("recipient-name").disabled = false;
     }
 }
