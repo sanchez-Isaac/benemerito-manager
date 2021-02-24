59850339d9404db833f602eac451752ddd6422a1
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-24 13:56:34 -0700

    testing chackbox disable

--- assets/js/editProfile.js
+++ assets/js/editProfile.js
@@ -1,5 +1,5 @@
 function disableMyText(){
-    if(document.getElementById("flexCheckUser_name").checked === true){
+    if(document.getElementById("flexCheckUser_name").checked === false){
         document.getElementById("recipient-name").disabled = "";
     }else if(document.getElementById("flexCheckUser_name").checked !== true){
         document.getElementById("recipient-name").disabled ="disable";
