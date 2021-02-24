7079321c96202f869e9d41f855e14d150a30b07c
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-24 15:28:46 -0700

    testing chackbox disable

--- assets/js/editProfile.js
+++ assets/js/editProfile.js
@@ -1,5 +1,7 @@
 //This will modify the modal active or inactive text in the sql changes.
 function disableMyTextName(textit,checkid){
+    console.log(textit);
+    console.log(checkid);
     if(document.getElementById(checkid).checked === false){
         document.getElementById(textit).disabled = "disable";
     }else if(document.getElementById(checkid).checked !== false){
