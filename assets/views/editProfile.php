4ffa551cdfbd8ea0af057d2bc200c137a2b13161
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-25 00:03:11 -0700

    Testing

--- assets/views/editProfile.php
+++ assets/views/editProfile.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include_once 'DbConnect.php';
+$con = get_db();
 
 
 
@@ -14,16 +15,44 @@ function pre_r($array)
 }
 
 if($_SESSION['recipient-names'] !== ""){
-    echo "Perfect";
+    if($_SESSION['message'] === "You are logged in Admin"){
+        $user_id = $_SESSION['user_id'];
+        $user_name = $_SESSION['recipient-names'];
+        $query = "UPDATE admin SET adm_name = '$user_id' WHERE admin_id = '$user_id'";
+        pg_query($con ,$query);
+        pg_close($con);
+    }
+    else if($_SESSION['message'] === "You are logged in Teacher"){
+
+        $user_id = $_SESSION['user_id'];
+        $user_name = $_SESSION['recipient-names'];
+        $query = "UPDATE teacher SET adm_name = '$user_id' WHERE teacher_id = '$user_id'";
+        pg_query($con ,$query);
+        pg_close($con);
+    }
+
+
+
+}
+if($_SESSION['recipient-middles'] !== ""){
+    echo "DO SQL PUSH";
+
+}
+if($_SESSION['recipient-births'] !== ""){
+    echo "DO SQL PUSH";
+
+}
+if($_SESSION['recipient-emails'] !== ""){
+    echo "DO SQL PUSH";
+
+}
+if($_SESSION['recipient-zooms'] !== ""){
+    echo "DO SQL PUSH";
 
 }
 
-//$_SESSION['recipient-names'];
-//$_SESSION['recipient-middles'];
-//$_SESSION['recipient-births'];
-//$_SESSION['recipient-emails'];
-//$_SESSION['recipient-zooms'];
 
 
 
-//header("Location: profile.php?edition=Success");
+
+header("Location: profile.php?edition=Success");
