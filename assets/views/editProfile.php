9d9b564679c9f473b64580ddf7ec7f57aebdb6f5
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-24 18:35:07 -0700

    Testing edit profile

--- assets/views/editProfile.php
+++ assets/views/editProfile.php
@@ -8,5 +8,21 @@ echo "This is working";
 
 }
 ?>
+<!doctype html>
 
-Test
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+<script src="js/scripts.js"></script>
+</body>
+</html>
