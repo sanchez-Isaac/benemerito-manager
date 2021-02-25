e3093c9eedf2d4983d821d944246a4400b24f235
Isaac Sanchez <idsm_2000@hotmail.com>
2021-02-24 22:46:22 -0700

    Testing

--- assets/views/profile.php
+++ assets/views/profile.php
@@ -19,18 +19,17 @@ echo '</pre>';
 
 
 if(isset($_POST['Submitting'])) {
-
-    //$strhtml = '<!doctype html>';
-    //$dochtml = new DOMDocument();
-   // $dochtml->loadHTML($strhtml);
-
+    $strhtml = '<!doctype html>';
+    $dochtml = new DOMDocument();
+    $dochtml->loadHTML($strhtml);
 
 
 
-   // if($dochtml->getElementById('recipient-name')->hasAttribute('disabled') === "" ){
+
+    if($dochtml->getElementById('recipient-name')->hasAttribute('disabled') === "" ){
 
         $_SESSION['recipient-names'] = pg_escape_string($_POST['recipient-names']);
-    //}
+    }
 
 
     $_SESSION['recipient-middles'] = pg_escape_string($_POST['recipient-middles']);
