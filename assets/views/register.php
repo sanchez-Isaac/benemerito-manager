65246bed60b0cd350eb9cec611cda344bd8ecc63
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-10 16:36:42 -0700

    btn edit

--- assets/views/register.php
+++ assets/views/register.php
@@ -112,7 +112,7 @@ echo '</pre>';
                 <div class="col-sm-12 col-md-5 col-lg-5 col-xl-5">
 
 
-                        <button class="btn btn-secondary btn-block" id="teacherbtn" type="button">Teachers</button>
+                        <button style="text-align: left;padding: 0.375rem 2.75rem;" class="btn btn-secondary btn-block" id="teacherbtn" type="button">Teachers</button>
 
                         <button class="btn btn-secondary btn-block" id="studentbtn" type="button">Students</button>
 
