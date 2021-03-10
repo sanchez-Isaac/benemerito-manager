5f26468e3bd3945940e4c77bc6c8399c9269c87a
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-10 16:32:55 -0700

    btn edit

--- assets/views/register.php
+++ assets/views/register.php
@@ -123,16 +123,7 @@ echo '</pre>';
                 <!-- Start: table for picture -->
                 <div class="col-md-5 col-lg-5 col-xl-5">
 
-                    <div class="topright">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140"  fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"></path>
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"></path>
-                        </svg>
-                        <br>
-                        <br>
-                        <h4 class="movepos"> <?php   echo $_SESSION['user_name']. " " . $_SESSION['middle_name']. " " . $_SESSION['last_name']; ?></h4>
-                        <h6>Admin</h6>
-                    </div>
+
 
                 </div><!-- End: table for picture -->
                 <div class="col-sm-12 col-md-1 col-lg-1 col-xl-1 offset-xl-0"></div>
