eacf9c8db431e1ba8285b8381540ee25b10717dc
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-10 20:12:37 -0700

    Editing registration front-end

--- assets/views/teacher-reg.php
+++ assets/views/teacher-reg.php
@@ -156,39 +156,47 @@ if(isset($_POST['Submitting'])) {
                     <tr>
                         <td><label for="inputName" class="form-labels">First Name:</label></td>
                         <td><input type="text" class="form-control" id="inputName" required></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td><label for="inputMiddle" class="form-labels">Middle Name:</label></td>
                         <td><input type="text" class="form-control" id="inputMiddle"></td>
-
+                        <td></td>
                     </tr>
                     <tr>
                         <td><label for="inputLast" class="form-labels">Last Name:</label></td>
                         <td><input type="text" class="form-control" id="inputLast" required></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td><label for="inputBirth" class="form-labels">Birthdate:</label></td>
                         <td> <input type="date" class="form-control" id="inputBirth" required></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td> <label for="inputEmail" class="form-labels">Email:</label></td>
                         <td> <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="you@example.com" required></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td><label for="inputZoom" class="form-labels">Zoom:</label></td>
                         <td><input type="url" class="form-control" id="inputZoom"></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td><label for="inputMPhone" class="form-labels">Mobile:</label></td>
                         <td><input type="tel" class="form-control" id="inputMPhone" required></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td><label for="inputPhone" class="form-labels">Telephone:</label></td>
                         <td><input type="tel" class="form-control" id="inputPhone"></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td><label for="inputPass" class="form-labels">Password:</label></td>
                         <td><input size="16" type="password" name="inputPass" class="form-control" id="inputPass" placeholder="*******" pattern=".{5,10}" required title="5 to 10 characters" ></td>
+                        <td></td>
                     </tr>
                 </table>
             </div><!-- End: registration Form-->
