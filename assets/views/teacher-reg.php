61a17ee3606b6812ee322d9e4bac467e5be60744
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-11 17:36:56 -0700

    Teesting btns

--- assets/views/teacher-reg.php
+++ assets/views/teacher-reg.php
@@ -226,8 +226,8 @@ if(isset($_POST['Submitting'])) {
         <div class="row">
             <!-- Start: cancel and Save btn-->
             <div class="col-md-12">
-                <button style="position: relative;" type="button" class="btn btn-success" id="addbtn">Add</button>
-                <button type="button" class="btn btn-danger"id="cancelbtn">Cancel</button>
+                <button style="position: absolute;" type="button" class="btn btn-success" id="addbtn">Add</button>
+                <button style="position: absolute;" type="button" class="btn btn-danger"id="cancelbtn">Cancel</button>
 
 
             </div><!-- end: cancel and Save btn-->
