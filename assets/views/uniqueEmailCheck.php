f61600ab985cd983c0dd9f03dbd96da38721e1ca
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-17 11:24:11 -0600

    Checking query for unique email

--- assets/views/uniqueEmailCheck.php
+++ assets/views/uniqueEmailCheck.php
@@ -23,3 +23,22 @@
 
     }
 
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Submit info</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+
+</body>
+</html>
