78415c64f052fed728ae7f648cf2721614bbd4e9
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-17 11:55:10 -0600

    Checking query for unique email

--- assets/views/uniqueEmailCheck.php
+++ assets/views/uniqueEmailCheck.php
@@ -14,17 +14,20 @@ include_once '../dbConnect/DbConnect.php';
     if (pg_num_rows($result) > 0) {
         while ($row = pg_fetch_array($result)) {
             if ($row = $email) {
-
+/*
                 echo '<script language="javascript">';
                 echo 'alert("Email Already taken")';
                 echo console_log("taken");
                 echo '</script>';
+*/
                 header("Location: add-teacher.php?emailTaken=Error");
             } else {
+                /*
                 echo '<script language="javascript">';
                 echo 'alert("Not taken")';
                 echo console_log("Not Taken");
                 echo '</script>';
+                */
                 header("Location: add-teacher.php?Approved=");
 
             }
