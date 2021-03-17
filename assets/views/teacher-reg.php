858b6953e5f58444e1729191adc347aafb70c1dc
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-17 10:46:28 -0600

    Checking query for unique email

--- assets/views/teacher-reg.php
+++ assets/views/teacher-reg.php
@@ -217,7 +217,7 @@ if(isset($_POST['Submitting'])) {
                         <td>&nbsp;</td>
                     </tr>
                     <tr>
-                        <form type="post">
+                        <form method="post">
                         <td> <label for="inputEmail" class="form-labels">Email:</label></td>
                         <td> <input type="email" class="form-control" name="add-recipient-emails" id="inputEmail" aria-describedby="emailHelp" placeholder="you@example.com" required>  </td>
                     <td><button type="button" value="checkValid" name="valid?" class="btn btn-primary" id="checkValidBtn" onclick="checkValid()">Validate</button></td></td>
