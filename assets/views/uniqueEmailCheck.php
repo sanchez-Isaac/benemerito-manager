f9ef6726367f8db367ea691b701ea3f663dc2b82
Isaac Sanchez <idsm_2000@hotmail.com>
2021-03-17 14:16:59 -0600

    Checking query for unique email in tables admin and teacher

--- assets/views/uniqueEmailCheck.php
+++ assets/views/uniqueEmailCheck.php
@@ -6,27 +6,25 @@ include_once '../dbConnect/DbConnect.php';
 
     $email = $_SESSION['add-teach-emails'];
 
-    $queryEmail = ("SELECT email FROM teacher WHERE email = '$email'");
+    //$queryEmail = ("SELECT email FROM teacher WHERE email = '$email'");
+
+$queryEmail = ("SELECT email FROM admin WHERE email = '$email' 
+                UNION 
+                SELECT email FROM teacher WHERE email = '$email';");
+
     $con3 = get_db();
+
 
 
 
     $result = pg_query($con3, $queryEmail);
     if (pg_num_rows($result) == 1) {
 
-
-                echo '<script language="javascript">';
-                echo 'alert("Email Already taken")';
-
-                echo '</script>';
                 $_SESSION['taken?'] = 'Taken';
                 header("Location: teacher-reg.php?emailTaken=Error");
             } else {
 
-                echo '<script language="javascript">';
-                echo 'alert("Not taken")';
 
-                echo '</script>';
                 $_SESSION['taken?'] = 'not Taken';
                 header("Location: add-teacher.php?Approved=");
 
